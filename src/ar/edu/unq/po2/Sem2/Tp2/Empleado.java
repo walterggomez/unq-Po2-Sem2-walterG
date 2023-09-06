550bd2ba23904227912bbe3746f9e14d5c5821da
walterggomez <walterggomez@gmail.com>
2023-09-06 00:51:10 -0300

    se trabaja sobre la clase Empleado

--- src/ar/edu/unq/po2/Sem2/Tp2/Empleado.java
+++ src/ar/edu/unq/po2/Sem2/Tp2/Empleado.java
@@ -1,5 +1,40 @@
 package ar.edu.unq.po2.Sem2.Tp2;
 
-public class Empleado {
+import java.time.LocalDate;
+import java.time.Period;
+
+public abstract class Empleado {
+	private String nombreEmpleado;
+	private String direccionEmpleado;
+	private String estadoCivil;
+	private LocalDate fechaDeNac;
+	private double sueldoBasico;
+	
+	
+	
+	
+	
+	
+	
+	public LocalDate getFechaDeNac() {
+		return fechaDeNac;
+	}
+
+	public int edad() {
+		LocalDate fechaHoy = LocalDate.now();
+		Period periodo = Period.between(getFechaDeNac(),fechaHoy);
+		return periodo.getYears();
+	}
+	
+	public abstract double sueldoBruto();
+	
+	public abstract double retenciones();
+	
+	public double sueldoNeto() {
+		return this.sueldoBruto()-this.retenciones();
+	}
+	
+	
+	
 
 }
---
+++ src/ar/edu/unq/po2/Sem2/Tp2/EmpleadoPermanente.java
@@ -0,0 +1,5 @@
+package ar.edu.unq.po2.Sem2.Tp2;
+
+public class EmpleadoPermanente {
+
+}
